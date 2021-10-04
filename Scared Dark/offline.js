4512e2c2462c0d69f91896ecea40891db45dbaba
Purple-heart-uno <57563924+Purple-heart-uno@users.noreply.github.com>
2021-10-04 23:27:08 +0400

    Add files via upload

---
+++ Scared Dark/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1633375555,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledsprite-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/idle-sheet0.png",
+		"images/idle-sheet1.png",
+		"images/block-sheet0.png",
+		"images/fire-sheet0.png",
+		"images/wood-sheet0.png",
+		"images/spawner-sheet0.png",
+		"images/light-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/fearbar-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
